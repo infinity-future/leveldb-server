5af66e56e4f1b701ee7e167e42c96899d145a8be
infinityfuture <infinityfuture@foxmail.com>
2018-08-20 00:23:54 +0800

    auto push docker image with tag

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,4 +1,5 @@
 
 sudo docker build --no-cache -t infinityfuture/leveldb-server:latest . && \
     sudo docker tag infinityfuture/leveldb-server:latest infinityfuture/leveldb-server:`cat version.txt` && \
+    sudo docker push infinityfuture/leveldb-server:`cat version.txt` && \
     sudo docker push infinityfuture/leveldb-server:latest
